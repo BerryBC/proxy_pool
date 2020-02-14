b23f3ac86ec640d0802445639488db56f1f60193
BerryBC <Berry_BC_@163.com>
2020-02-14 15:32:50 +0800

    更新链接

--- class/IOEng/cIOMongodb.js
+++ class/IOEng/cIOMongodb.js
@@ -17,7 +17,7 @@ class cMongodbIO {
         that.dbSet = objDBSet;
         that.strMGUrl = 'mongodb://' + that.dbSet.user + ':' + that.dbSet.pw + '@localhost:' + that.dbSet.host + '/' + that.dbSet.dbName;
         that.clientMongo = null;
-        MongoClient.connect(that.strMGUrl, { poolSize: 100 }, function(err, client) {
+        MongoClient.connect(that.strMGUrl, { poolSize: 50, keepAlive: false, reconnectTries: 100, reconnectInterval: 30 }, function(err, client) {
             if (!err) {
                 that.clientMongo = client;
             } else {
